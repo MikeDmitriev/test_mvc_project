7b640313ca79e746f8af82c1c53d827354f40226
MADmitriev <MADmitriev@bellintegrator.ru>
2017-06-08 10:38:36 +0300

    Refactoring. Delete comments in servlet controller, change .gitignore.

--- src/main/java/ru/bellintegrator/myapp/BankServletController/BankServletControllerImpl/BankServletControllerImpl.java
+++ src/main/java/ru/bellintegrator/myapp/BankServletController/BankServletControllerImpl/BankServletControllerImpl.java
@@ -32,85 +32,85 @@ public class BankServletControllerImpl implements BankServletController {
     }
 
     @Override
-    @RequestMapping(value = "/new_request", consumes = APPLICATION_JSON_VALUE, /*produces = APPLICATION_JSON_VALUE,*/ method = POST)
+    @RequestMapping(value = "/new_request", consumes = APPLICATION_JSON_VALUE, method = POST)
     public String createCardRequest(@RequestBody CardRequestView cardRequestView) {
         dbService.createNewCardRequest(cardRequestView.owner, cardRequestView.bankCard);
         return "ok";
     }
 
     @Override
-    @RequestMapping(value = "/all_request", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/all_request", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<CardRequestView> viewAllCardRequest() {
         return dbService.getAllRequests();
     }
 
     @Override
-    @RequestMapping(value = "/request", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/request", produces = APPLICATION_JSON_VALUE, method = GET)
     public CardRequestView viewCardRequest(@RequestParam long requestId) {
         return dbService.getCardRequest(requestId);
     }
 
     @Override
-    @RequestMapping(value = "/owner_request", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/owner_request", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<CardRequestView> viewAllOwnersCardRequests(@RequestParam short passportSerial, @RequestParam int passportNum) {
         return dbService.getAllRequestByOwner(passportSerial, passportNum);
     }
 
     @Override
-    @RequestMapping(value = "/owner_account", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/owner_account", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<AccountView> viewOwnersAccounts(@RequestParam short passportSerial, @RequestParam int passportNum) {
         return dbService.getAllAccountsByOwner(passportSerial, passportNum);
     }
 
     @Override
-    @RequestMapping(value = "/owner_card", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/owner_card", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<BankCardView> viewOwnersCards(@RequestParam short passportSerial, @RequestParam int passportNum) {
         return dbService.getAllCardsByOwner(passportSerial, passportNum);
     }
 
     @Override
-    @RequestMapping(value = "/card", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/card", produces = APPLICATION_JSON_VALUE, method = GET)
     public BankCardView viewCard(@RequestParam long cardId) {
         return dbService.getCardById(cardId);
     }
 
     @Override
-    @RequestMapping(value = "/sign_request", /*consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE,*/ method = GET)
+    @RequestMapping(value = "/sign_request", method = GET)
     public boolean signCardRequest(@RequestParam long requestId) {
         return dbService.signRequest(requestId);
     }
 
     @Override
-    @RequestMapping(value = "/send_request", /*consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE,*/ method = GET)
+    @RequestMapping(value = "/send_request", method = GET)
     public boolean sendCardRequest(@RequestParam long requestId) {
         return dbService.sendRequest(requestId);
     }
 
     @Override
-    @RequestMapping(value = "/ready_request", /*consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE,*/ method = GET)
+    @RequestMapping(value = "/ready_request", method = GET)
     public boolean readyCardRequest(@RequestParam long requestId) {
         return dbService.readyRequest(requestId);
     }
 
     @Override
-    @RequestMapping(value = "/activate", /*consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE,*/ method = GET)
+    @RequestMapping(value = "/activate", method = GET)
     public boolean activateCard(@RequestParam long cardId) {
         return dbService.activateCard(cardId);
     }
 
     @Override
-    @RequestMapping(value = "/accounts", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/accounts", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<AccountView> getAllAccounts(){
         return dbService.getAllAccounts();
     }
 
     @Override
-    @RequestMapping(value = "/owners", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/owners", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<OwnerView> getAllOwners(){
         return dbService.getAllOwners();
     }
 
-    @RequestMapping(value = "/owners_id", /*consumes = APPLICATION_JSON_VALUE,*/ produces = APPLICATION_JSON_VALUE, method = GET)
+    @RequestMapping(value = "/owners_id", produces = APPLICATION_JSON_VALUE, method = GET)
     public List<OwnerIdView> getAllOwnersIds(){
         return dbService.getAllOwnersId();
     }
